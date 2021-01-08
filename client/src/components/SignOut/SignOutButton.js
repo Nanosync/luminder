c1549f9b2bb93bdcbf73248aaf5789e47a316c84
Nanosync <10258208+Nanosync@users.noreply.github.com>
2021-01-08 23:17:08 +0800

    fix sign out sizing

--- client/src/components/SignOut/SignOutButton.js
+++ client/src/components/SignOut/SignOutButton.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import { withFirebase } from "../Firebase";
 import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SignOutButton = ({ firebase }) => (
-  <FontAwesomeIcon icon={faSignOutAlt} onClick={firebase.doSignOut} style={{color: "white"}}/>
+  <Link to="#" title="Sign out" alt="Sign out"><FontAwesomeIcon icon={faSignOutAlt} onClick={firebase.doSignOut} style={{color: "white"}} size="2x"/></Link>
 );
 
 export default withFirebase(SignOutButton);
