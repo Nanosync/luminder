24b3a1b79e9f2d1599062b9885447ca43969aa0f
Samuel Fang <samuelfangjw@gmail.com>
2021-01-08 14:40:23 +0800

    refactored App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
@@ -11,24 +11,34 @@ import Chat from './pages/Chat';
 import NotFound from './pages/NotFound';
 import Reset from './pages/Reset';
 
-function App() {
-  return (
-    <Router>
-      <div className="d-flex flex-column min-vh-100">
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/register" exact component={Register} />
-          <Route path="/profile" exact component={Profile} />
-          <Route path="/chat" exact component={Chat} />
-          <Route path="/reset" exact component={Reset} />
-          <Route component={NotFound} />
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authUser: null,
+    };
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="d-flex flex-column min-vh-100">
+          <Header />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/register" exact component={Register} />
+            <Route path="/profile" exact component={Profile} />
+            <Route path="/chat" exact component={Chat} />
+            <Route path="/reset" exact component={Reset} />
+            <Route component={NotFound} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
