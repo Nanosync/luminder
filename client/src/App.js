2a1d810112694da6cd3a7c3b0768a5ff38f06e14
Nanosync <10258208+Nanosync@users.noreply.github.com>
2021-01-08 22:55:38 +0800

    fix logout

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,6 @@ import Chat from "./pages/Chat";
 import NotFound from "./pages/NotFound";
 import Reset from "./pages/Reset";
 import Edit from "./pages/EditProfile";
-import Logout from "./pages/Logout";
 import { withAuthentication } from "./components/Session/";
 
 class App extends Component {
@@ -28,7 +27,6 @@ class App extends Component {
             <Route path="/chat" exact component={Chat} />
             <Route path="/reset" exact component={Reset} />
             <Route path="/edit" exact component={Edit} />
-            <Route path="/logout" exact component={Logout} />
             <Route component={NotFound} />
           </Switch>
           <Footer />
