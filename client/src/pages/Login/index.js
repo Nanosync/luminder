3dd8587461f0ab35fbfd0da4245d8b753929d3b2
Nanosync <10258208+Nanosync@users.noreply.github.com>
2021-01-08 23:05:33 +0800

    Fix auth

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import { Container, Row, Col, Button, Image } from "react-bootstrap";
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+import { AuthUserContext } from '../../components/Session';
 import LoginComponent from '../../components/Login';
 
 const Login = () => {
+  const user = useContext(AuthUserContext);
+  const history = useHistory();
+
+  if (user) {
+    history.push("/");
+    return null;
+  }
+  
   return (
     <Container className="flex-grow-1 d-flex align-items-center justify-content-center">
       <Row>
