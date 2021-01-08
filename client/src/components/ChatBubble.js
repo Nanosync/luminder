ad45793a10c9e3ddd5621fea5b10928d6d13dda2
Samuel Fang <samuelfangjw@gmail.com>
2021-01-08 22:53:37 +0800

    abstracted out chat bubbles

--- client/src/components/ChatBubble.js
+++ client/src/components/ChatBubble.js
@@ -1,30 +1,48 @@
-import React from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
+import React from "react";
+import "./ChatBubble.css";
 
-const ChatList = ({chatList}) => {
-  console.log(chatList);
-  return (
-    <>
-      <h1>Chats</h1>
-      <ListGroup>
-        {chatList.map((chat, index) => (
-          <ListGroup.Item action href={`#${index}`} key={index}>
-            <div className="d-flex">
-              <div className="chat-user-photo mr-2">
-                <img src={chat.recipients[0]} width="64px" height="64px" alt="User Avatar" />
-              </div>
-              <div>
-                <div className="d-flex w-100 justify-content-between">
-                  <h5 className="mb-1">TEST</h5>
-                </div>
-                <p className="chat-text mb-1">{chat.messages[chat.messages.length - 1].message}</p>
-              </div>
-            </div>
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
-    </>
-  );
+const ChatBubble = (props) => {
+  if (props.direction === "left") {
+    return (
+      <div className="d-flex flex-row flex-nowrap">
+        <div className="chat-user-photo mr-2">
+          <img
+            src={props.photo}
+            width="64px"
+            height="64px"
+            alt="User Avatar"
+            roundedCircle
+          />
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">{props.name}</h5>
+          </div>
+        </div>
+        <div className="chat-bubble">
+          <p className="chat-text mb-1">{props.message}</p>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="d-flex flex-row flex-nowrap">
+        <div className="chat-bubble-right">
+          <p className="chat-text mb-1">{props.message}</p>
+        </div>
+        <div className="chat-user-photo mr-2">
+          <img
+            src={props.photo}
+            width="64px"
+            height="64px"
+            alt="User Avatar"
+            roundedCircle
+          />
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">{props.name}</h5>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
-export default ChatList;
+export default ChatBubble;
