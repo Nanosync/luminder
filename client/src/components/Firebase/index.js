4eaa891223829b0ddee23c06cb057b523b9924a1
shiminnt <shishishishimin@gmail.com>
2021-01-09 09:21:35 +0800

    edit

--- client/src/components/Firebase/index.js
+++ client/src/components/Firebase/index.js
@@ -1,22 +1,7 @@
 import FirebaseContext, { withFirebase } from './context';
 import Firebase from './firebase';
-import firebase from "firebase/app";
 import "firebase/storage";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB-vjxnYrYcWElEsoJw7wO09BlZOiqVnE4",
-    authDomain: "luminder-4341b.firebaseapp.com",
-    databaseURL: "https://luminder-4341b-default-rtdb.firebaseio.com",
-    projectId: "luminder-4341b",
-    storageBucket: "luminder-4341b.appspot.com",
-    messagingSenderId: "829281562599",
-    appId: "1:829281562599:web:f94dbd43ec248c258ad742"
-  };
-
-firebase.initializeApp(firebaseConfig);
-
-const storage = firebase.storage();
- 
-export {storage, firebase as default };
+export default Firebase;
  
 export { FirebaseContext, withFirebase };
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
-import ReactDOM, { render } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Firebase, { FirebaseContext } from './components/Firebase';
-import { storage } from './firebase';
 
 
 ReactDOM.render(
@@ -16,26 +15,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-const ReactFirebaseFileUpoad = () => {
-  const [image, setImage] = useState(null);
-
-  const handleChange = e => {
-    if (e.target.files[0]) {
-
-    }
-  };
-
-  const handleUpload = () => {};
-
-  return (
-    <div>
-      Hi all!
-      <br />
-    </div>
-  );
-};
-
-render(<ReactFirebaseFileUpoad />, document.querySelector("#root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
